f95f7fc6662129d58713124d21a69049f2f0e2e8
BinaryBard <shrikhandeaniruddha@gmail.com>
2023-02-09 22:35:09 +0530

    codeforces day 1

---
+++ flipFlop.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class flipFlop{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test-->0) {
+            int n = sc.nextInt();
+            int[] array = new int[n];
+            for (int j = 0; j < n; j++) {
+                array[j] = sc.nextInt();
+            }
+            soln(array);
+        }
+        sc.close();
+    }
+    public static void negateElement(int arr[], int i) {
+        arr[i] = -arr[i];
+        arr[i + 1] = -arr[i + 1];
+
+    }
+    public static void soln(int[] array){
+        int sum = 0;
+        boolean flag = false;
+        if (array.length == 2) {
+            negateElement(array, 0);
+            sum = array[0]+array[1];
+        }
+        else {
+            for (int z = 0; z < array.length-1; z++) {
+                if (array[z] == -1 && array[z+1] == -1) {
+                    if (!flag) {
+                        negateElement(array, z);
+                    }
+                    flag = true;
+                }
+                sum += array[z];
+            }
+            sum+=array[array.length-1];
+        }
+        System.out.println(sum);
+    }
+
+}
